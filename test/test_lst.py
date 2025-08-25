70168925f72e37fe86f37ee43bde02e128f92f61
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2025-08-25 19:58:47 +0200

    Register and send an event to an event listener

--- test/test_lst.py
+++ test/test_lst.py
@@ -3,11 +3,11 @@ from typing import Callable
 from pytest import raises
 
 # Todo:
-# - ability to register a new observer
 # - ability to call the listener, and when that happens, the observers should be called
 # - ability to pass some argument or an event from the caller to the observer
 #
 # Done:
+# - ability to register a new observer
 # - if trying to register an observer that's not callable, throw exception
 # - if trying to call a listener that's not registered, throw exception
 
@@ -22,12 +22,23 @@ def test_if_trying_to_register_an_observer_that_is_not_callable__throw_exception
     with raises(NotCallable):
         listener.add('key', not_callable)
 
+def test_register_a_new_observer():
+    listener = EventListener()
+    listener.add('registered', lambda: None)
+    listener.send('registered')
+
 class EventListener[Key]:
+    def __init__(self):
+        self.observers: list[Key] = []
+
     def add(self, key: Key, observer: Callable):
-        raise NotCallable()
+        if not callable(observer):
+            raise NotCallable()
+        self.observers.append(key)
 
     def send(self, key: Key):
-        raise NotRegistered()
+        if key not in self.observers:
+            raise NotRegistered()
 
 class NotRegistered(Exception):
     pass
